cabdb710a65a44ad912a77c02dbd2ff229986e95
EricWRogers <ericwilliamrogers@gmail.com>
2023-02-02 23:11:29 -0600

    atlas demo

--- src/ECS/ScriptableEntities/BeachBall.hpp
+++ src/ECS/ScriptableEntities/BeachBall.hpp
@@ -9,17 +9,30 @@ class BeachBall : public Canis::ScriptableEntity
 private:
     glm::vec2   m_direction;
     float       m_speed;
+    float       m_timeBetweenAnimation = 3.0f;
+    float       m_countDown = 0.0f;
+    unsigned int m_animIndex = 0;
     std::vector<glm::vec2> m_spawnPoints = {};
 public:
     void OnCreate()
     {
-        
+        m_countDown = m_timeBetweenAnimation;
     }
 
     void OnReady()
     {
         m_direction = glm::vec2(1.0f, 0.4f);
         m_speed = 150.0f;
+        auto& sprite = GetComponent<Canis::Sprite2DComponent>();
+            GetSpriteFromTextureAtlas(
+                sprite,
+                m_animIndex,
+                0,
+                48,
+                50,
+                true,
+                false
+            );
     }
     
     void OnDestroy()
@@ -28,7 +41,30 @@ public:
     }
 
     void OnUpdate(float _dt)
-    {
+    {        
+        m_countDown -= _dt;
+
+        if (m_countDown < 0.0f)
+        {
+            m_countDown = m_timeBetweenAnimation;
+
+            m_animIndex++;
+
+            if (m_animIndex > 5)
+                m_animIndex = 0;
+
+            auto& sprite = GetComponent<Canis::Sprite2DComponent>();
+            GetSpriteFromTextureAtlas(
+                sprite,
+                m_animIndex,
+                0,
+                48,
+                50,
+                true,
+                true
+            );
+        }
+
         auto& rect = GetComponent<Canis::RectTransformComponent>();
 
         float halfSizeX = rect.size.x/2.0f;
