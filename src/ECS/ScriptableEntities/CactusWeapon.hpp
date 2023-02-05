7c08e026bb31746ae74fcad02544d4199d62e92e
Connor Ballew <conmon119@gmail.com>
2023-02-04 19:11:04 -0600

    Cactus Weapon Beginning

---
+++ src/ECS/ScriptableEntities/CactusWeapon.hpp
@@ -0,0 +1,39 @@
+#pragma once
+#include <string>
+#include <Canis/ScriptableEntity.hpp>
+#include <Canis/ECS/Components/CircleColliderComponent.hpp>
+
+class CactusWeapon : public WeaponClass
+{
+public:
+
+    float damage = 10.0f;
+
+    void OnCreate()
+    {
+        
+    }
+
+    void OnReady()
+    {
+
+    }
+    
+    void OnDestroy()
+    {
+
+    }
+
+    void OnUpdate(float _dt)
+    {
+       std::vector<entt::entity> hit = GetSystem<Canis::CollisionSystem2D>()->GetHits(m_Entity.entityHandle);
+        Canis::Entity hitEntity;
+
+        for(int i; i <= hit.size(); i++)
+        {
+            hitEntity.entityHandle = hit[i];
+            hitEntity.GetComponent<EnemySpawnManager>().health -= damage;
+
+        }
+    }
+};
